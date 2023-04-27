ccf65470f3444d30cca01ec82f8e92d5280ef285
Duvan Castro <duvancastro1026@gmail.com>
2023-04-27 04:15:56 -0500

    add table roles

--- data/GestionUsuario.sql
+++ data/GestionUsuario.sql
@@ -1,6 +1,16 @@
-CREATE TABLE tipodocumento(
+-- Create Table for the document type
+CREATE TABLE tipodocumento(  
 	id_documento SERIAL PRIMARY KEY,
 	nombre_documento character(60)
 )
+
+-- Create Table for the roles
+CREATE TABLE roles(   		
+	id_roles serial PRIMARY KEY,
+	nombre_rol character(5)
+	
+)
+
+
 
 SELECT * FROM TipoDocumento
