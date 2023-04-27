f600bc04c5c180f91b8bcf881d79a075659c97f3
duvan1026 <duvancastro1026@gmail.com>
2023-04-27 13:36:26 -0500

    create table usuario, usuarios_roles and roles

--- data/GestionUsuario.sql
+++ data/GestionUsuario.sql
@@ -1,8 +1,36 @@
 -- Create Table for the document type
-CREATE TABLE tipodocumento(  
-	id_documento serial PRIMARY KEY NOT NULL,
+CREATE TABLE tipo_documento(  
+	oid_documento serial PRIMARY KEY NOT NULL,
 	nombre_documento character(60) NOT NULL
 )
+
+-- Create Table of the user
+CREATE TABLE usuarios(
+	oid_usuario bigserial PRIMARY KEY NOT NULL,
+	nombres_usuario character(50),
+	apellidos_usuario character(50),
+	celular character(15),
+	email character(80) NOT NULL,
+	contraseña character(80) NOT NULL,
+	fecha_creacion date NOT NULL,
+	fecha_nacimiento date NOT NULL,
+	fk_oid_documento serial NOT NULL REFERENCES tipo_documento(oid_documento)	
+)
+
+
+-- create table rol
+CREATE TABLE roles(
+	oid_rol serial PRIMARY KEY NOT NULL,
+	nombre_rol character(10) NOT NULL
+)
+
+-- create table roles
+CREATE TABLE usuarios_roles(
+	oid_usuario_roles serial PRIMARY KEY NOT NULL,
+	oid_usuario bigserial NOT NULL REFERENCES usuarios(oid_usuario),
+	oid_rol serial NOT NULL REFERENCES roles(oid_rol)
+)
+
 
 -- Create Table for the roles
 CREATE TABLE roles(   		
@@ -10,18 +38,7 @@ CREATE TABLE roles(
 	nombre_rol character(5)	NOT NULL
 )
 
--- Create Table of the user
-CREATE TABLE usuarios(
-	id_usuario bigserial PRIMARY KEY NOT NULL,
-	nombres_usuario character(50),
-	apellidos_usuario character(50),
-	celular character(15),
-	email character(80) NOT NULL,
-	contraseña character(80) NOT NULL,
-	fecha_creacion date NOT NULL,
-	fk_roles_id serial NOT NULL REFERENCES roles(id_roles),	
-	fk_documento_id serial NOT NULL REFERENCES tipodocumento(id_documento)	
-)
+
 
 
 -- check  of the tables
