eedbbe5f96fa6259cc86ad8559ec857f38e5a0e7
Duvan Castro <duvancastro1026@gmail.com>
2023-04-27 04:35:47 -0500

    create table usuarios

--- data/GestionUsuario.sql
+++ data/GestionUsuario.sql
@@ -1,16 +1,30 @@
 -- Create Table for the document type
 CREATE TABLE tipodocumento(  
-	id_documento SERIAL PRIMARY KEY,
-	nombre_documento character(60)
+	id_documento serial PRIMARY KEY NOT NULL,
+	nombre_documento character(60) NOT NULL
 )
 
 -- Create Table for the roles
 CREATE TABLE roles(   		
-	id_roles serial PRIMARY KEY,
-	nombre_rol character(5)
-	
+	id_roles serial PRIMARY KEY NOT NULL,
+	nombre_rol character(5)	NOT NULL
+)
+
+-- Create Table of the user
+CREATE TABLE usuarios(
+	id_usuario bigserial PRIMARY KEY NOT NULL,
+	nombres_usuario character(50),
+	apellidos_usuario character(50),
+	celular character(15),
+	email character(80) NOT NULL,
+	contraseña character(80) NOT NULL,
+	fecha_creacion date NOT NULL,
+	fk_roles_id serial NOT NULL REFERENCES roles(id_roles),	
+	fk_documento_id serial NOT NULL REFERENCES tipodocumento(id_documento)	
 )
 
 
 
 SELECT * FROM TipoDocumento
+SELECT * FROM roles
+SELECT * FROM usuarios
