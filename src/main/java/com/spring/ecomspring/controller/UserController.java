e9cefc40b3dc29df0a74d0fd5251315db8684982
duvan1026 <duvancastro1026@gmail.com>
2023-05-08 10:52:35 -0500

    create controller finAll() in UserController

--- src/main/java/com/spring/ecomspring/controller/UserController.java
+++ src/main/java/com/spring/ecomspring/controller/UserController.java
@@ -1,4 +1,29 @@
 package com.spring.ecomspring.controller;
 
+import com.spring.ecomspring.entities.User;
+import com.spring.ecomspring.repository.UserRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class UserController {
+
+    private final UserRepository userRepository;
+
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/Users")
+    public ResponseEntity<List<User>> findAll(){
+
+        List<User> users = userRepository.findAll();
+        return  ResponseEntity.ok(users);
+    }
 }
