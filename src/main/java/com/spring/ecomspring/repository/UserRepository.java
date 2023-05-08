47edeeb16b70023453cdce13c3004f2c6263593e
duvan1026 <duvancastro1026@gmail.com>
2023-05-08 10:39:05 -0500

    create file interface UserRepository

---
+++ src/main/java/com/spring/ecomspring/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.spring.ecomspring.repository;
+
+
+import com.spring.ecomspring.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
