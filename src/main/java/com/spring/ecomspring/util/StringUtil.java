ed8fb9c87a3ac99b0d8a4e45aaacc25291c4b12b
Duvan Castro <duvancastro1026@gmail.com>
2023-05-10 05:00:41 -0500

    create method trim for the String

--- src/main/java/com/spring/ecomspring/util/StringUtil.java
+++ src/main/java/com/spring/ecomspring/util/StringUtil.java
@@ -1,2 +1,34 @@
-package com.spring.ecomspring.util;public class StringUtil {
+package com.spring.ecomspring.util;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringUtil {
+
+    /**
+     * Elimina los espacios vacíos al principio y al final de una cadena de texto
+     *
+     * @param str cadena de texto a limpiar
+     * @return String cadena de texto sin espacios al principio y al final
+     */
+    public static String trim(String str){
+        return str == null ? null : str.trim();
+    }
+
+    /**
+     * Elimina los espacios vacios al principio y al final de una lista de String
+     *
+     * @param list lista de cadena de texto a limpiar
+     * @return List<String> lista de cadena de texto sin espacios vacíos al principio y al final
+     */
+    public static List<String> trim(List<String> list){
+        List<String> trimmedList = new ArrayList<>();
+        for( String str: list ){
+            trimmedList.add(trim(str));
+        }
+        return trimmedList;
+    }
+
+
 }
