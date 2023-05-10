6905ab470a46cf96a220491766597a142a56678a
duvan1026 <duvancastro1026@gmail.com>
2023-05-10 12:04:13 -0500

    implement controller documentType

--- src/main/java/com/spring/ecomspring/controller/DocumentTypeController.java
+++ src/main/java/com/spring/ecomspring/controller/DocumentTypeController.java
@@ -1,4 +1,24 @@
 package com.spring.ecomspring.controller;
 
+import com.spring.ecomspring.entities.DocumentType;
+import com.spring.ecomspring.repository.DocumentTypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class DocumentTypeController {
+
+    @Autowired
+    private DocumentTypeRepository documentTypeRepository;
+
+    @GetMapping("/DocumentType")
+    public ResponseEntity<List<DocumentType>> findAll(){
+
+        List<DocumentType> documentTypes = documentTypeRepository.findAll();
+        return  ResponseEntity.ok(documentTypes);
+    }
 }
