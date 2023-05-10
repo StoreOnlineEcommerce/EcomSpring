9a86d6aa46c4acf983c007d6547f9423ba4cd86b
duvan1026 <duvancastro1026@gmail.com>
2023-05-10 14:00:57 -0500

    implement filter for the getdocumentType

--- src/main/java/com/spring/ecomspring/controller/DocumentTypeController.java
+++ src/main/java/com/spring/ecomspring/controller/DocumentTypeController.java
@@ -2,6 +2,7 @@ package com.spring.ecomspring.controller;
 
 import com.spring.ecomspring.entities.DocumentType;
 import com.spring.ecomspring.repository.DocumentTypeRepository;
+import com.spring.ecomspring.services.DocumentTypeImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,10 +16,13 @@ public class DocumentTypeController {
     @Autowired
     private DocumentTypeRepository documentTypeRepository;
 
+    @Autowired
+    private DocumentTypeImpl documentTypeImpl;
+
     @GetMapping("/DocumentType")
     public ResponseEntity<List<DocumentType>> findAll(){
 
-        List<DocumentType> documentTypes = documentTypeRepository.findAll();
+        List<DocumentType> documentTypes = documentTypeImpl.getfindAll();
         return  ResponseEntity.ok(documentTypes);
     }
 }
--- src/main/java/com/spring/ecomspring/services/DocumentTypeImpl.java
+++ src/main/java/com/spring/ecomspring/services/DocumentTypeImpl.java
@@ -1,6 +1,36 @@
 package com.spring.ecomspring.services;
 
+import com.spring.ecomspring.entities.DocumentType;
+import com.spring.ecomspring.repository.DocumentTypeRepository;
+import com.spring.ecomspring.util.StringUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class DocumentTypeImpl {
+
+    @Autowired
+    private DocumentTypeRepository documentTypeRepository;
+
+    @Autowired
+    private StringUtil stringUtil;
+
+    public List<DocumentType> getfindAll(){
+
+        List<DocumentType> documentTypes = documentTypeRepository.findAll();
+
+        for(DocumentType document : documentTypes){
+
+            String name_InSpaces = stringUtil.trim(document.getName());
+            document.setName(name_InSpaces);
+        }
+
+        return documentTypes;
+    }
+
 
 
 }
