97bf9989351f256f027a26f9c71a3e5fb5569f01
Duvan Castro <duvancastro1026@gmail.com>
2023-05-12 06:33:58 -0500

    implement interface of the spring repository methods

--- src/main/java/com/spring/ecomspring/services/IUserService.java
+++ src/main/java/com/spring/ecomspring/services/IUserService.java
@@ -1,4 +1,33 @@
 package com.spring.ecomspring.services;
 
+
+import com.spring.ecomspring.entities.User;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IUserService {
+
+    // spring repository methods
+
+    List<User> findAll();
+
+    Optional<User> finById();
+
+    User save(User user);
+
+    User update(User user);
+
+    void deleteById(Long id);
+
+    void deleteById(List<Long> ids);
+
+    void deleteAll();
+
+    void deleteAll(List<User> users);
+
+
+
+
+
 }
