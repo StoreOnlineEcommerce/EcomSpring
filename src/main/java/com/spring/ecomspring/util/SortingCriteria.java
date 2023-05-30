09b16ff8dbe178b7e1b3a31c6d36ad894a0bccc8
Duvan Castro <duvancastro1026@gmail.com>
2023-05-30 09:45:56 -0500

    create class SortingCriteria

--- src/main/java/com/spring/ecomspring/util/SortingCriteria.java
+++ src/main/java/com/spring/ecomspring/util/SortingCriteria.java
@@ -1,4 +1,29 @@
 package com.spring.ecomspring.util;
 
-public class SortingCriteria {
+import org.springframework.data.domain.Sort;
+
+public class SortingCriteria<T> {
+    private String property;
+    private Sort.Direction direction;
+
+    public SortingCriteria(String property, Sort.Direction direction) {
+        this.property = property;
+        this.direction = direction;
+    }
+
+    public String getProperty() {
+        return property;
+    }
+
+    public void setProperty(String property) {
+        this.property = property;
+    }
+
+    public Sort.Direction getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Sort.Direction direction) {
+        this.direction = direction;
+    }
 }
